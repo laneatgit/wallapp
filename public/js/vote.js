f1bc116089a24ca4e857f625664d74c9e03caef6
laneatgit <laneatgit@gmail.com>
2015-07-21 14:25:57 +0900

    ui update 20150721

--- public/js/vote.js
+++ public/js/vote.js
@@ -1,13 +1,18 @@
 function addVote(sender,id,vote_rank) {
 
+    if ($(sender).hasClass('fa-disabled'))
+    {
+        return;
+    }
+    
     var votes =parseInt($(sender).text());
     var url = "/posts/" + id + "/up_vote";
-    var icon = "<i class='fa fa-smile-o faa-tada'></i>";
+    var icon = "<i class='fa fa-smile-o'></i>";
     var sign = "+";
     if (vote_rank === -1)
     {
         url = "/posts/" + id + "/down_vote";
-        icon = "<i class='fa fa-frown-o faa-tada'></i>";
+        icon = "<i class='fa fa-frown-o'></i>";
         sign = "";
     }
     $.ajax({
@@ -15,7 +20,7 @@ function addVote(sender,id,vote_rank) {
 	data: "",
 	type: "POST",
 	beforeSend: function(){
-		$(sender).html("<img src='/img/ajax-loader.gif' />");
+       $(sender).html('<i class="fa fa-refresh fa-spin"></i>');
 	},
 	success: function(){
         switch(vote_rank) {
@@ -25,12 +30,16 @@ function addVote(sender,id,vote_rank) {
             case -1:
             votes = votes-1;
             break;
-        }   
-        $(sender).html(icon + sign + votes.toString());	
+        }
+        setTimeout(function() {
+           
+            $(sender).html(icon + sign + votes.toString());
+            $(sender).addClass('fa-disabled');
+        },200);	
 	}
 	});
 }
-
+/*
 function submitComment(sender,post_id) {
     
     // form.serialize() does not include hidden value, so must to add it manually.
@@ -48,7 +57,7 @@ function submitComment(sender,post_id) {
 
 	}
 	});
-}
+}*/
 
 function findPostContainer(post_id) {
     return $(".post[data-id='" + post_id + "']");
@@ -58,7 +67,9 @@ function showComment(sender,post_id) {
 
     var url = '/posts/' + post_id + '/comments';
     var parent = $('#post_' + post_id + '_comment_area');
-    var comment_region_visible = $(sender).data("comments-visible") || false;    
+    var comment_region_visible = $(sender).data("comments-visible") || false; 
+    var $icon = $('#post-id-' + post_id + '  .fa-comment').parent();   
+    var icon_html =  $icon.html();  
     if (comment_region_visible === false)
     { 
         // load comments
@@ -66,10 +77,15 @@ function showComment(sender,post_id) {
             url: url,
             data: "",
             dataType: "html",
+            beforeSend: function(){
+               $icon.html('<i class="fa fa-refresh fa-spin"></i>');
+            },
             success: function(html){
-                parent.append(html);
-                $.data(sender, 'comments-visible', true);
-                
+                setTimeout( function() {
+                    parent.append(html);
+                    $.data(sender, 'comments-visible', true);
+                    $icon.html(icon_html);
+                }, 200);
             }
         });
     } else {
